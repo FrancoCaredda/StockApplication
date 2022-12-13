899f63a493c3a61054971fe621de58e91d88123b
FrancoCaredda <franco.caredda12345@gmail.com>
2022-12-13 10:44:24 +0100

    Filtering implementation

--- webapp/controller/Products.controller.js
+++ webapp/controller/Products.controller.js
@@ -1,12 +1,33 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function(Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, Filter, FilterOperator, JSONModel) {
     'use strict';
     
     return Controller.extend("stock.application.controller.Products", {
         _tableExpanded: false,
+        _searchCutList: [
+            "Total",
+            "All"
+        ],
+        _filter: function(binding, filterOperator, property, value) {
+            let filter = null;
+
+            if (value && value.length > 0) {
+                filter = new Filter(property, filterOperator, value);
+            }
+
+            binding.filter(filter, "Products");
+        },
         onInit: function() {
-            this.getOwnerComponent().getModel("products")
+            
+        },
+        onAfterRendering: function() {
+            // const binding = this.getView().byId("products-table").getBinding("items");
+
+            // this._filter(binding, FilterOperator.EQ, "Type", "Console");
         },
         expandTable: function(event) {
             const control = event.getSource();
@@ -29,7 +50,7 @@ sap.ui.define([
         },
         moveToProduct: function(event) {
             const router = this.getOwnerComponent().getRouter();
-            const model = event.getSource().getBindingContext("products");
+            const model  = event.getSource().getBindingContext("products");
             console.log(model);
 
             if (model != undefined) {
@@ -37,6 +58,25 @@ sap.ui.define([
                     product: window.encodeURIComponent(model.getPath())
                 })
             }
+        },
+        searchFieldChanged: function(event) {
+            const binding = this.getView().byId("products-table").getBinding("items");
+            
+            this._filter(binding, FilterOperator.Contains, "Name", event.getSource().getValue())
+        },
+        typeChanged: function(event) {
+            const binding = this.getView().byId("products-table").getBinding("items");
+
+            this._filter(binding, FilterOperator.EQ, "Type", event.getSource().getSelectedItem().getText())
+        },
+        generateSearchList: function(id, context) {
+            const element = this.getView().byId("search-list-item-type");
+
+            if (!this._searchCutList.includes(context.getProperty("TypeName"))) {
+                return element.clone(id);
+            }
+
+            return undefined;
         }
     })
 });
